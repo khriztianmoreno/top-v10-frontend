aadc575f9d09b1bc34b3e5df1442c66ed037a1bd
Juan <juanan0309@gmail.com>
2021-09-02 18:23:45 -0500

    Add update logic and styles

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import axios from "axios";
 export default function App() {
   const [tasks, setTasks] = useState([]);
   const [state, setState] = useState({
-    title: ""
+    title: "",
   });
 
   useEffect(() => {
     axios
       .get("http://localhost:3001/tasks/")
       .then((response) => setTasks(response.data))
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }, []);
 
   function updateTitle(e) {
     setState({
       ...state,
-      title: e.target.value
+      title: e.target.value,
     });
   }
 
@@ -39,47 +39,60 @@ export default function App() {
   function saveTask() {
     axios
       .post("http://localhost:3001/tasks", {
-        title: state.title
+        title: state.title,
       })
       .then((response) => {
         setTasks((tasks) => tasks.concat(response.data));
         setState((state) => ({
           ...state,
-          title: ""
+          title: "",
         }));
       });
   }
 
   function deleteTask(e, task) {
     e.preventDefault(); // prevenimos el comportamiento por defecto
-    axios.delete(`http://localhost:3001/tasks/${task.id}`)
-      .then(() => setTasks(tasks.filter(t => t.id !== task.id)))
-      .catch(err => console.log(err))
+    axios
+      .delete(`http://localhost:3001/tasks/${task.id}`)
+      .then(() => setTasks(tasks.filter((t) => t.id !== task.id)))
+      .catch((err) => console.log(err));
   }
 
-  function toggleTask(idx) {
-    setTasks(
-      tasks.map((task, i) => (i === idx ? { ...task, done: !task.done } : task))
-    );
+  function toggleTask(task) {
+    axios
+      .put(`http://localhost:3001/tasks/${task.id}`, {
+        data: {
+          ...task,
+          completed: !task.completed,
+        },
+      })
+      .then(() => {
+        setTasks(
+          tasks.map((t) =>
+            +task.id === t.id ? { ...task, completed: !task.completed } : t
+          )
+        );
+      });
   }
 
   return (
     <div className="App">
       <h1>Lista de Tareas</h1>
       <ul className="tasks">
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <span
-              className={task.completed ? "done" : null}
-              onClick={() => toggleTask(task)}
-            >
-              {task.title}
-            </span>{" "}
-            <a href="#" onClick={(e) => deleteTask(e, task)}>
-              x
-            </a>
-          </li>
-        ))}
+        {tasks.length > 0 &&
+          tasks.map((task) => (
+            <li key={task.id}>
+              <span
+                className={task.completed ? "done" : null}
+                onClick={() => toggleTask(task)}
+              >
+                {task.title}
+              </span>{" "}
+              <a href="#" onClick={(e) => deleteTask(e, task)}>
+                x
+              </a>
+            </li>
+          ))}
       </ul>
       <input type="text" value={state.title} onChange={updateTitle} />
       <button onClick={saveTask}>Crear Tarea</button>
