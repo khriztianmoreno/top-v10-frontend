93d350038efdab2abd2c6eecd19b2adad80c3320
Germán Escobar <german.escobarc@gmail.com>
2021-09-14 14:36:09 -0500

    Refactor PrivateRoute

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -4,11 +4,11 @@ import { LOADING, AUTHENTICATED } from './constants'
 
 const PrivateRoute = ({component: Component, ...rest}) => {
   const auth = useSelector(state => state.auth)
+
+  if (auth.status === LOADING) return <p>Loading ....</p>
   return (
     <Route {...rest} render={props => (
-      auth.status === AUTHENTICATED
-      ? <Component {...props} />
-      : (auth.status === LOADING ? <p>Loading ....</p> : <Redirect to="/login" />)
+      auth.status === AUTHENTICATED ? <Component {...props} /> : <Redirect to="/login" />
     )} />
   );
 };
