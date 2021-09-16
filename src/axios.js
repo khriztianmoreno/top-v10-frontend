9e906b50ff41b35fe4be2182718dcf7e73c22420
Germán Escobar <german.escobarc@gmail.com>
2021-09-16 10:09:15 -0500

    Add env variable BACKEND_URL

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { TOKEN } from './constants'
 
 const customAxios = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.BACKEND_URL || "http://localhost:3001",
   headers: {
     'Authorization': localStorage.getItem(TOKEN)
   }
